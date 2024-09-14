0bedbf868d13bb49bb3999677acf34575b653563
pooyapr81 <Pooya.parva81@gmail.com>
2024-09-14 12:01:52 +0330

    register and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login_view'),
+    path('logout/', views.logout_view, name='logout_view'),
+    path('register/', views.register_view, name='register_view'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import AccountAuthenticationForm
+from .forms import AccountAuthenticationForm, RegistrationForm
 from django.contrib.auth import authenticate, login, logout
 
 
@@ -17,7 +18,7 @@ def login_view(request):
         form = AccountAuthenticationForm(request.POST)
         if form.is_valid():
             print('form', form.cleaned_data)
-            email = form.cleaned_data.get('email')
+            email = form.cleaned_data.get('email').lower()
             raw_password = form.cleaned_data.get('password')
             user = authenticate(email=email, password=raw_password)
             if user:
@@ -29,3 +30,36 @@ def login_view(request):
     context['login_form'] = form
 
     return render(request, 'account/login.html', context)
+
+
+def register_view(request, *args, **kwargs):
+    user = request.user
+    if user.is_authenticated:
+        return HttpResponse("You are already authenticated as " + str(user.email))
+
+    context = {}
+    if request.POST:
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email').lower()
+            raw_password = form.cleaned_data.get('password1')
+            account = authenticate(email=email, password=raw_password)
+            login(request, account)
+            destination = kwargs.get("next")
+            if destination:
+                return redirect(destination)
+            messages.success(request, 'register successfully', 'success')
+            return redirect("accounts:home")
+        else:
+            context['registration_form'] = form
+
+    else:
+        form = RegistrationForm()
+        context['registration_form'] = form
+    return render(request, 'account/register.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('accounts:home')
